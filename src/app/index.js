8e4f578da097f30fcc68e60f9d823ef65245d660
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-05-15 23:42:32 +0530

    updated the header material components

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import HeaderViewContainer from '../components/header/HeaderView';
+import { HeaderView } from '../components/header/HeaderView';
 import SpotifyContainer from './spotify/Container';
 
 class App extends Component {
@@ -10,15 +8,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MuiThemeProvider>
-          <main>
-            <HeaderViewContainer />
-            <Switch>
-              <Route exact path="/spotify" component={SpotifyContainer} />
-              <Redirect from="/" to="spotify" />
-            </Switch>
-          </main>
-        </MuiThemeProvider>
+        <main>
+          <HeaderView />
+          <Switch>
+            <Route exact path="/spotify" component={SpotifyContainer} />
+            <Redirect from="/" to="spotify" />
+          </Switch>
+        </main>
       </div>
     );
   }
